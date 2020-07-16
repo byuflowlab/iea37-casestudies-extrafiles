58abefd552b240c61eac4c6d18b9fa8dc4669178
jaredthomas68 <jaredthomas68@gmail.com>
2020-07-16 09:45:55 -0600

    add bash script

---
+++ cs3-4/jared-files/bashexopt2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --time=01:00:00   # walltime
+#SBATCH --ntasks=4   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=4   # number of nodes
+#SBATCH --mem-per-cpu=1024M   # memory per CPU core
+#SBATCH -J "test flowfarm distributed "   # job name
+#SBATCH --qos=test
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+julia example_opt_2_distributed.jl

b03641e0f717776a8202a6928b8d5d20aaeec657
jaredthomas68 <jaredthomas68@gmail.com>
2020-07-16 12:49:42 -0600

    adjust practice script to more closely resemble actual job

--- cs3-4/jared-files/distributed_practice.jl
+++ cs3-4/jared-files/distributed_practice.jl
@@ -2,8 +2,8 @@ using Distributed
 using ClusterManagers
 
 
-addprocs(SlurmManager(parse(Int, ENV["SLURM_NTASKS"])-1))
-# addprocs(4)
+# addprocs(SlurmManager(parse(Int, ENV["SLURM_NTASKS"])-1))
+addprocs(0)
 # hosts = []
 # pids = []
 # for i in workers()
@@ -12,13 +12,14 @@ addprocs(SlurmManager(parse(Int, ENV["SLURM_NTASKS"])-1))
 #         push!(hosts, host)
 #         push!(pids, pid)
 # end
-c = @sync @distributed (+) for i = 1:nworkers()
+t1 = time()
+c = @sync @distributed (+) for i = 1:10
 
     sleep(1)
     b = 1
 
 end
-
+t2 = time()
 
 # The Slurm resource allocation is released when all the workers have
 # exited
@@ -26,4 +27,5 @@ for i in workers()
         rmprocs(i)
 end
 
+println("ran in ", t2-t1)
 println(c)

3e6631ce7d5a897403d37a7331eb5a973966590b
wesleyjholt <64283549+wesleyjholt@users.noreply.github.com>
2020-08-24 06:51:31 -0600

    change opt tol

--- cs3-4/wesley-files/opt-files/example_opt_4_ieacs4_WEC_discrete.jl
+++ cs3-4/wesley-files/opt-files/example_opt_4_ieacs4_WEC_discrete.jl
@@ -181,8 +181,8 @@ for i = 1:2, j = 1:nturbines
     x[(i-1)*nturbines+j] = initial_yaml["definitions"]["position"]["items"][j][i]
 end
 xopt_all[:,1] = [deepcopy(x[1:nturbines]);deepcopy(x[nturbines+1:end])]
-tol = 2.5e-1 # 1.5e-1 2.0e-1 2.5e-1 3.0e-1
-layout_number = 251
+tol = 2.0e-1 # 1.5e-1 2.0e-1 2.5e-1 3.0e-1
+layout_number = 201
 
 # set globals for iteration history
 funcalls_AEP_WEC = zeros(Float64, 50000*noptimizations)

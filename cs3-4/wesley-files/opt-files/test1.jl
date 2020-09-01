98b68d6d37f04d326191bd5ef5288b9aa664fe3b
wesleyjholt <64283549+wesleyjholt@users.noreply.github.com>
2020-09-01 14:07:12 -0600

    update test

--- cs3-4/wesley-files/opt-files/test1.jl
+++ cs3-4/wesley-files/opt-files/test1.jl
@@ -1,2 +1,4 @@
 layout_number = Base.parse(Int, ENV["SLURM_ARRAY_TASK_ID"])
-println(layout_number)
+println("First argument is: ", Base.ARGS[1])
+println("Second argument is: ", Base.ARGS[2])
+println("Layout number is: ", layout_number)

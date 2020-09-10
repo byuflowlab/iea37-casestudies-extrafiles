1fb3917fc978979048cd4603915b4fedfbdc9de4
Wesley Holt <wesleyjholt@gmail.com>
2020-09-10 10:19:54 -0600

    add bash scripts for ieacs4 opt

--- cs3-4/wesley-files/opt-files/ieacs4_distributed_fullwindrose_laststep.sh
+++ cs3-4/wesley-files/opt-files/ieacs4_distributed_fullwindrose_laststep.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --ntasks=12
+#SBATCH --ntasks=200
 #SBATCH --mem-per-cpu=2048M  # memory per CPU core
 #SBATCH --time=08:00:00
-#SBATCH -J 'ieacs4 - WEC with reduced wind rose 110'
+#SBATCH -J 'ieacs4 - WEC with full wind rose 110'
 #SBATCH --mail-user=wesleyjholt@gmail.com   # email address
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
@@ -11,7 +11,7 @@
 # #SBATCH --qos=test
 
 LAYOUT_NUMBER=110
-TOL=3.8e-6
+TOL=2.6e-6
 
 module load julia
-julia example_opt_4_ieacs4_WEC_discrete.jl ${LAYOUT_NUMBER} ${TOL}
+julia example_opt_4_ieacs4_discrete_fullwindrose2.jl ${LAYOUT_NUMBER} ${TOL}

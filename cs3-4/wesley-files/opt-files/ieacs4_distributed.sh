17924ac511e831c8d26b98b7f79cd4be483cb606
Wesley Holt <wesleyjholt@gmail.com>
2020-09-10 10:22:41 -0600

    change layout value in bash script

--- cs3-4/wesley-files/opt-files/ieacs4_distributed.sh
+++ cs3-4/wesley-files/opt-files/ieacs4_distributed.sh
@@ -3,14 +3,14 @@
 #SBATCH --ntasks=12
 #SBATCH --mem-per-cpu=2048M  # memory per CPU core
 #SBATCH --time=08:00:00
-#SBATCH -J 'ieacs4 - WEC with reduced wind rose 110'
+#SBATCH -J 'ieacs4 - WEC with reduced wind rose 103'
 #SBATCH --mail-user=wesleyjholt@gmail.com   # email address
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 # #SBATCH --array=1
 # #SBATCH --qos=test
 
-LAYOUT_NUMBER=110
+LAYOUT_NUMBER=103
 TOL=3.8e-6
 
 module load julia
--- cs3-4/wesley-files/opt-files/ieacs4_distributed_fullwindrose_laststep.sh
+++ cs3-4/wesley-files/opt-files/ieacs4_distributed_fullwindrose_laststep.sh
@@ -3,14 +3,14 @@
 #SBATCH --ntasks=200
 #SBATCH --mem-per-cpu=2048M  # memory per CPU core
 #SBATCH --time=08:00:00
-#SBATCH -J 'ieacs4 - WEC with full wind rose 110'
+#SBATCH -J 'ieacs4 - WEC with full wind rose 103'
 #SBATCH --mail-user=wesleyjholt@gmail.com   # email address
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 # #SBATCH --array=1
 # #SBATCH --qos=test
 
-LAYOUT_NUMBER=110
+LAYOUT_NUMBER=103
 TOL=2.6e-6
 
 module load julia
